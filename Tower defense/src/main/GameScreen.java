47b24afee70ad28472576b2135134df4c71709cc
FranFranJams <SuperNewComputer16@outlook.com>
2021-08-03 00:44:25 +0100

    Create GameScreen.java

---
+++ Tower defense/src/main/GameScreen.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameScreen extends JPanel {
+
+	public GameScreen() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	
+		
+		
+		
+	}
+	
+}
