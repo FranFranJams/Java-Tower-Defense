9664bada18209c2bba7e164a4d95ccd245978989
FranFranJams <SuperNewComputer16@outlook.com>
2021-08-03 13:07:39 +0100

    Added stuff
    + Temp Spritesheet
    + FPS counter
    + UPS counter
    + Game Loop
    + Start Thread

--- Tower defense/src/main/Game.java
+++ Tower defense/src/main/Game.java
@@ -1,31 +1,132 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Game extends JFrame{
-	
+public class Game extends JFrame implements Runnable {
 	
 	private GameScreen gameScreen;
+	
+	private BufferedImage img;
+	
+	
+	private int updates;
+	private long lastTimeUPS;
+	
+	private long lastUpdate;
+	private double timePerUpdate;
+	
+	private Thread gameThread;
 	
 	//Use Ctrl + Space to show auto complete!
 	
 	public Game() {
+
+		
+		timePerUpdate = 1000000000.0 / 60.0;
+		
+		importImg();
+		
 		
 		setSize(640,640);
-		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setTitle("Tower Defense");
 		
 		
-		gameScreen = new GameScreen();
+		gameScreen = new GameScreen(img);
 		add(gameScreen);
+		setVisible(true);
 	}
 	
 	
+	private void importImg() {
+		
+		InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+		
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	
+	}
+	
+	
+	private void start() {
+		gameThread = new Thread(this) {};
+		
+		gameThread.start();
+	}
+
+	private void loopGame() {
+ 
+		}
+			
+	private void callUPS() {
+		if (System.currentTimeMillis() - lastTimeUPS >=1000) {
+			System.out.println("UPS: " + updates);
+			updates = 0;
+			lastTimeUPS = System.currentTimeMillis();
+		}
+	}
+	
+
+	private void updateGame() {
+		updates++;
+		lastUpdate = System.nanoTime();
+		//System.out.println("Game Updated");
+		
+	}
+
 	public static void main(String[] args) {
 				
 		Game game = new Game();
+		game.start();
+	}
+	
+	@Override
+	public void run() {
+	
+
+		double timePerFrame;
+		long lastFrame = System.nanoTime();
+		
+		//long lastUpdate;
+		//double timePerUpdate;
+		
+		timePerFrame = 1000000000.0 / 100.0;
+		//timePerUpdate = 1000000000.0 / 60.0;
+	
+		while(true) {
+			if (System.nanoTime() - lastFrame >= timePerFrame) {
+				lastFrame = System.nanoTime();
+				repaint();		
+	}		
+
+			//Render
+		
+		if (System.nanoTime() - lastUpdate >= timePerUpdate) {
+				updateGame();
+				
+				callUPS();
+			}
+		}			
+
+		
+		
+	//Update
+		
+	//Checking FPS + UPS
 		
 	}
+	
+	
+	
+	
 }
--- Tower defense/src/main/GameScreen.java
+++ Tower defense/src/main/GameScreen.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -10,22 +12,73 @@ public class GameScreen extends JPanel {
 
 	private Random random;
 	
+	private BufferedImage img;
+	
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+	
+	private long lastTime;
+	
+	private int frames;
+	
+
+	
+	
+	public GameScreen(BufferedImage img) {
+		this.img = img;
 		
-	public GameScreen() {
-	 random = new Random();	
+		loadSprites();
+		
+		random = new Random();	
+		
+		
+
 	}
 	
+	private void loadSprites() {
+		
+		for (int y = 0; y < 10; y++) {
+			for (int x = 0; x < 10; x++) {
+				sprites.add(img.getSubimage(x*32, y*32, 32, 32));
+			}
+		}
+		
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-	
+		
+	//	g.drawImage(sprites.get(8), 0, 0, null);
+		//g.drawImage(img.getSubimage(32*9, 32, 32, 32), 0, 0, null);
+		
 		for (int y = 0; y < 20; y++) {
 			for (int x = 0; x < 20; x++) {
-				g.setColor(getRandColor());
+				
+				g.drawImage(sprites.get(getRandInt()), x*32, y*32, null);
+				
+				
+				//g.setColor(getRandColor());
 				//g.setColor(new Color(108,45,199));
-				g.fillRect(x * 32, y * 32, 32, 32);
+				//g.fillRect(x * 32, y * 32, 32, 32);
 		}
 	}
+
+		callFPS();
+	
 }
+	
+	private void callFPS() {
+		frames++;
+		if (System.currentTimeMillis() - lastTime >= 1000) {
+			System.out.println("FPS: " + frames);
+			frames = 0;
+			lastTime = System.currentTimeMillis();
+		}
+	}
+	
+	
+	private int getRandInt() {
+		return random.nextInt(100);
+	}
 	
 	
 	private Color getRandColor() {
